c229723f4e11e2a108dfe2e601fa61102bd912ad
Gabriel Bursi <gabriellbursii@gmail.com>
2023-10-17 17:12:20 -0300

    config react-query

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,23 +4,29 @@ import { PropsWithChildren } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { GlobalStyles, theme } from '../styles'
 import StyledComponentsRegistry from '../lib/registry'
 
 export function Providers({ children }: PropsWithChildren) {
+
+	const queryClient = new QueryClient()
+
 	return (
 		<>
-			<CacheProvider>
-				<ChakraProvider>
-					<GlobalStyles />
-					<StyledComponentsRegistry>
-						<ThemeProvider theme={theme}>
-							{children}
-						</ThemeProvider>
-					</StyledComponentsRegistry>
-				</ChakraProvider>
-			</CacheProvider>
+			<QueryClientProvider client={queryClient}>
+				<CacheProvider>
+					<ChakraProvider>
+						<GlobalStyles />
+						<StyledComponentsRegistry>
+							<ThemeProvider theme={theme}>
+								{children}
+							</ThemeProvider>
+						</StyledComponentsRegistry>
+					</ChakraProvider>
+				</CacheProvider>
+			</QueryClientProvider>
 		</>
 	)
 }
